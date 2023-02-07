bac29320f593ca65b4f86ac302933632f2498a16
marcos <msanchezm@iesch.org>
2023-02-07 15:50:53 +0100

    first commit

---
+++ src/main/java/APIConsume/ApiConsumeApplication.java
@@ -0,0 +1,13 @@
+package APIConsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsumeApplication.class, args);
+	}
+
+}
